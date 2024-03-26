05446ead081f756d287f655781e7373daa453577
Thomas Thomas <thomas.thomas@diamond.ac.uk>
2024-03-26 14:14:17 +0000

    Initial Play

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,22 @@
-// import { LogComponent } from './components/Card';
+
 import LogTable from './components/Table';
 import './App.css'
 import {theme} from "./theme";
 import { ThemeProvider } from '@emotion/react';
+
 function App() {
   
   const data = "Garry"
 
     return (
       <ThemeProvider theme={theme}>
-      <h1 className='Athena Logpanel'>
-        <p>Hello World</p>
+      <p className='Athena Logpanel'>
+        <h2>Hello World</h2>
         <button className='square'>1</button>
         <label htmlFor='Log1'></label>
-        {/* <LogComponent></LogComponent> */}
-        <p> {data} </p>
         <LogTable></LogTable>
-      </h1>
+        <h3> {data} </h3>
+      </p>
       </ThemeProvider>
       )
   }
