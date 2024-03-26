48a81c3873def55366996632ad80b470d155fe64
Thomas Thomas <thomas.thomas@diamond.ac.uk>
2024-03-26 14:14:17 +0000

    Working MUI Example
    Using MUI box example to render logs inside.

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+
+  // server: {
+  //   configureServer: ({ middlewares }) => {
+  //     middlewares.use((req, res, next) => {
+  //       // Set CORS headers
+  //       res.setHeader('Access-Control-Allow-Origin', '*');
+  //       res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  //       res.setHeader('Access-Control-Allow-Headers', '*');
+  //       next();
+  // });
+  //   }}
 })
